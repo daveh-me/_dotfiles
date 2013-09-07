6329d5d695ec1506c1a1e6d26f594b40bfef4445
Dave Hooper <dave@daveh.me>
2013-09-07 15:23:43 +1200

    testing github cloning

--- setup.sh
+++ setup.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 source_dir="$HOME/.dotfiles"
 target_dir="$HOME"
+vim_bundle_dir="$source_dir/vim/bundle"
 timestamp=`date +%Y%m%d%H%M%S`
 
 cd $source_dir
 
-for link_source in `cat link-sources.txt`
-do
-    target="$HOME/.$link_source"
-    if [ -f $target ] || [ -L $target ]
-    then
-        mv -v $target $target.$timestamp
-    fi
-    ln -sv $source_dir/$link_source $target
-done
+#for link_source in `cat link-sources.txt`
+#do
+#    target="$HOME/.$link_source"
+#    if [ -f $target ] || [ -L $target ]
+#    then
+#        mv -v $target $target.$timestamp
+#    fi
+#    ln -sv $source_dir/$link_source $target
+#done
 
+git_sources=`cat github-sources.txt`
+cd $vim_bundle_dir
+rm -rfv *
+for git_source in $git_sources
+do
+    git clone $git_source
+done
