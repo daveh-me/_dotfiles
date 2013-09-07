2e99caa8cd45ae5fb38b87bdd01c070d5a62b300
Dave Hooper <dave@daveh.me>
2013-09-07 15:27:13 +1200

    reenable the linking

--- setup.sh
+++ setup.sh
@@ -6,16 +6,17 @@ timestamp=`date +%Y%m%d%H%M%S`
 
 cd $source_dir
 
-#for link_source in `cat link-sources.txt`
-#do
-#    target="$HOME/.$link_source"
-#    if [ -f $target ] || [ -L $target ]
-#    then
-#        mv -v $target $target.$timestamp
-#    fi
-#    ln -sv $source_dir/$link_source $target
-#done
+for link_source in `cat link-sources.txt`
+do
+    target="$HOME/.$link_source"
+    if [ -f $target ] || [ -L $target ]
+    then
+        mv -v $target $target.$timestamp
+    fi
+    ln -sv $source_dir/$link_source $target
+done
 
+cd $source_dir
 git_sources=`cat github-sources.txt`
 cd $vim_bundle_dir
 rm -rfv *
